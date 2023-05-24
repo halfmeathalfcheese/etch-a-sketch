0060567aa4d352884b299fa7b10a89495b141ac3
tim <z5361150@unsw.edu.au>
2023-05-24 16:58:54 +1000

    Completed all intended functionality, still has minor bug where sometimes mouse will drag, causing it to stay mousedown when it has been released

---
+++ app.js
@@ -0,0 +1,60 @@
+let isMousePressed = false;
+
+const defaultGridSize = 16;
+
+function createGrid(gridSize) {
+    const gridContainer = document.querySelector('.grid-container');
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const gridBlock = document.createElement('div');
+        gridBlock.classList.add('grid-block');
+        gridBlock.id = i;
+        gridBlock.style.flexBasis = `calc(100% / ${gridSize})`
+        gridBlock.style.height = `height: calc(100% / ${gridSize})`
+        gridContainer.appendChild(gridBlock);
+    }
+}
+
+function changeGridColour(event) {
+    if (isMousePressed || event.type === 'click') {
+        this.style.backgroundColor = 'red';
+    }
+}
+
+function handleMouseDown() {
+    isMousePressed = true;
+}
+
+function handleMouseUp() {
+    isMousePressed = false;
+}
+
+function changeGridSize() {
+    let newSize = prompt('Please enter the new size of grid.');
+    const gridBlocks = document.querySelectorAll('.grid-block');
+
+    gridBlocks.forEach(gridBlock => {
+        gridBlock.remove();
+    });
+
+    createGrid(newSize);
+
+    const newGridBlocks = document.querySelectorAll('.grid-block');
+    newGridBlocks.forEach(newGridBlock => {
+        newGridBlock.addEventListener('mouseenter', changeGridColour);
+        newGridBlock.addEventListener('mousedown', handleMouseDown);
+        newGridBlock.addEventListener('mouseup', handleMouseUp);
+        newGridBlock.addEventListener('click', changeGridColour);
+    });
+}
+
+createGrid(defaultGridSize);
+const changeGridButton = document.querySelector('#change-grid');
+changeGridButton.addEventListener('click', changeGridSize);
+const gridBlocks = document.querySelectorAll('.grid-block');
+gridBlocks.forEach(gridBlock => {
+    gridBlock.addEventListener('mouseenter', changeGridColour);
+    gridBlock.addEventListener('mousedown', handleMouseDown);
+    gridBlock.addEventListener('mouseup', handleMouseUp);
+    gridBlock.addEventListener('click', changeGridColour);
+})
